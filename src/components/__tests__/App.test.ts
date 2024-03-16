9530508154da2dece4c7acd0feae96e0c5013251
Jérôme Abel <contact@jeromeabel.net>
2024-03-16 09:18:15 +0100

    chore: add types for happy-dom

--- src/components/__tests__/App.test.ts
+++ src/components/__tests__/App.test.ts
@@ -2,4 +2,9 @@ describe('Tests', () => {
   it('should be running correctly', () => {
     expect(true).toBe(true)
   })
+
+  test('use happy-dom in this test file', () => {
+    const element = document.createElement('div')
+    expect(element).not.toBeNull()
+  })
 })
